3785f6a4c52044d20303245c39521f1d66cfcc50
Pratap Keshri <pratap.keshri@in.ibm.com>
2020-07-13 22:30:23 +0530

    commit

--- loginservice/src/main/java/com/ibm/loginservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.loginservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ibm.loginservice.entity.User;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserName(String username);
-}
